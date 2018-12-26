80ef8a32f31814e6400d8e6061a89d92c2a0b054
Your Name <you@example.com>
2018-12-26 09:05:15 +0800

    _2018_1226__0905am

--- test01_local/main.c
+++ test01_local/main.c
@@ -3,114 +3,118 @@
  * \author  		JasonLee
  * \date    		2018.08.30
  * \Version 		1.1
- * ADC Convert With Polling AIN0 or Internal 1/4 VDD eight times then calculate average.
+ * ADC Convert With Polling AIN4 or Internal 1/4 VDD eight times then calculate average.
  * 1. Set ADC clock frequency is 250KHz , Sample pulse width is 1 ADC clock,  
  *	  ADC conversion time = (1+12+2)*4us = 60us , ADC conversion rate = 1/60us = 16.6KHz
- * 2. Polling one of PA0(AIN0) or internal 1/4 VDD as ADC analog input eight times then calculate average.
- * 3. Store AIN0 channel ADC convert result bit11~ bit0 to RAM "_R_AIN0_DATA[11:0]"
- * 4. Store internal 1/4VDD channel ADC convert result bit11~ bit0 to RAM "_R_Quarter_VDD_DATA[11:0]"
+ * 2. Polling one of PA4(AIN4) or internal 1/4 VDD as ADC analog input eight times then calculate average.
+ * 3. Store AIN4 channel ADC convert result bit11~ bit0 to RAM "_R_pin_adc_dataBit_411[11:0]"
+ * 4. Store internal 1/4VDD channel ADC convert result bit11~ bit0 to RAM "_R_Quarter_VDD_dataBit_411[11:0]"
  */
 
 #include <at8.h>
 #include "at8_constant.h"
-unsigned int  _R_AIN0_DATA;	
-unsigned char _R_AIN0_DATA_LB;			
-unsigned int  _R_Quarter_VDD_DATA;	
-unsigned char _R_Quarter_VDD_DATA_LB;	
+unsigned int  _R_pin_adc_dataBit_411;	
+unsigned char _R_pin_adc_dataBit_0_3;			
+unsigned int  _R_Quarter_VDD_dataBit_411;	
+unsigned char _R_Quarter_VDD_dataBit_0_3;	
 
 #define UPDATE_REG(x)	__asm__("MOVR _" #x ",F")
 
-void _F_AIN0_Convert(char);
-void _F_Quarter_VDD_Convert(char);
-void _F_wait_eoc(void);
-void _delay(int);
+void _FadcRead_pin(char);
+void _FadcRead_Quarter_VDD(char);
+void _F_wait_adc_conver_end(void);
+void _FdelayMS(int);
+void _FmainLoop(void);
 
 void main(void)
 {
-    
-  //----- Initial GPIO-----
+
+    //----- Initial GPIO-----
     IOSTA = C_PA_Input;						// Set PortA as input port
     PORTA = 0xFF;							// PortA Data Register = 0xFF
     INTE  = 0x00;							// INTE = 0x00
 
- //----- Initial ADC-----	  
-	ADMD  = C_ADC_En | C_ADC_CH_Dis | C_ADC_PA0 ;	// Enable ADC power, Disable global ADC input channel, Select PA0 pad as ADC input (SFR "ADMD")
- 
- //----- ADC high reference voltage source select-----
- 	//ADVREFH = C_Vrefh_4V;					// ADC reference high voltage is supplied by internal 4V  (Note: ADC clock freq. must be equal or less 1MHz)
- 	//ADVREFH = C_Vrefh_3V;					// ADC reference high voltage is supplied by internal 3V  (Note: ADC clock freq. must be equal or less 500KHz)
- 	ADVREFH = C_Vrefh_2V;					// ADC reference high voltage is supplied by internal 2V  (Note: ADC clock freq. must be equal or less 250KHz)
- 
-//----- ADC clock frequency select----------------------------	 
-	//ADR	 = C_Ckl_Div1;					// ADC clock=Fcpu/1, Clear ADIF, disable ADC interrupt	
-	//ADR	 = C_Ckl_Div2;					// ADC clock=Fcpu/2, Clear ADIF, disable ADC interrupt	
-	ADR	  = C_Ckl_Div8;						// ADC clock=Fcpu/8, Clear ADIF, disable ADC interrupt	
-	//ADR	 = C_Ckl_Div16;					// ADC clock=Fcpu/16, Clear ADIF, disable ADC interrupt	
- 
-//----- ADC Sampling pulse width select-------------	 
- 	ADCR  = C_Sample_1clk | C_12BIT;		// Sample pulse width=1 adc clock, ADC select 12-bit conversion ( Note: ADC clock freq. must be equal or less 500KHz)
- 	//ADCR  = C_Sample_2clk | C_12BIT;		// Sample pulse width=2 adc clock, ADC select 12-bit conversion ( Note: ADC clock freq. must be equal or less 1MHz)
- 	//ADCR  = C_Sample_4clk | C_12BIT;		// Sample pulse width=4 adc clock, ADC select 12-bit conversion ( Note: ADC clock freq. must be equal or less 1.25MHz)
- 	//ADCR  = C_Sample_8clk | C_12BIT; 		// Sample pulse width=8 adc clock, ADC select 12-bit conversion ( Note: ADC clock freq. must be equal or less 2MHz)	
+    //----- Initial ADC-----	  
+    ADMD  = C_ADC_En | C_ADC_CH_Dis | C_ADC_PA4 ;	// Enable ADC power, Disable global ADC input channel, Select PA4 pad as ADC input (SFR "ADMD")
 
-//--------------------------------------------------	
-	PACON = C_PA0_AIN0;						// Set AIN0(PA0) as pure ADC input for reduce power consumption (SFR "PACON")
-	ADMDbits.GCHS = 1;						// Enable global ADC channel	(SFR "ADMD")
-	_delay(50);								// Delay 0.56ms(Instruction clock=4MHz/2T) waiting ADC stable 
+    //----- ADC high reference voltage source select-----
+    ADVREFH = C_Vrefh_2V;					// ADC reference high voltage is supplied by internal 2V  (Note: ADC clock freq. must be equal or less 250KHz)
+
+    //----- ADC clock frequency select----------------------------	 
+    ADR	  = C_Ckl_Div8;						// ADC clock=Fcpu/8, Clear ADIF, disable ADC interrupt	
+
+    //----- ADC Sampling pulse width select-------------	 
+    ADCR  = C_Sample_1clk | C_12BIT;		// Sample pulse width=1 adc clock, ADC select 12-bit conversion ( Note: ADC clock freq. must be equal or less 500KHz)
+
+    //--------------------------------------------------	
+    PACON = C_PA4_AIN4;						// Set AIN4(PA4) as pure ADC input for reduce power consumption (SFR "PACON")
+    ADMDbits.GCHS = 1;						// Enable global ADC channel	(SFR "ADMD")
+    _FdelayMS(50);								// Delay 0.56ms(Instruction clock=4MHz/2T) waiting ADC stable 
     while(1)
     {
-        CLRWDT();							// Clear WatchDog
-		_R_AIN0_DATA=_R_AIN0_DATA_LB=_R_Quarter_VDD_DATA=_R_Quarter_VDD_DATA_LB=0x00;            
-        _F_AIN0_Convert(8);					// executing AIN0 ADC converting 8 times
-        _R_AIN0_DATA <<= 4;					// _R_AIN0_DATA shift left 4 bit
-        _R_AIN0_DATA_LB &= 0xF0;				// Only get Bit7~4
-        _R_AIN0_DATA += _R_AIN0_DATA_LB;		// _R_AIN0_DATA + _R_AIN0_DATA_LB
-        _R_AIN0_DATA >>=3;					// _R_AIN0_DATA dividing 8
-        
-        _F_Quarter_VDD_Convert(8);			// executing 1/4VDD input channel ADC converting 8 times
-        _R_Quarter_VDD_DATA <<= 4;			// _R_Quarter_VDD_DATA shift left 4 bit
-        _R_Quarter_VDD_DATA_LB &= 0xF0;		// Only get Bit7~4
-        _R_Quarter_VDD_DATA += _R_Quarter_VDD_DATA_LB; //// _R_Quarter_VDD_DATA + _R_Quarter_VDD_DATA_LB
-        _R_Quarter_VDD_DATA >>=3;			// _R_Quarter_VDD_DATA dividing 8
+        _FmainLoop();
+    }
+} // main()
+
+void _FmainLoop(void)
+{
+    CLRWDT();							// Clear WatchDog
+    //_R_pin_adc_dataBit_411=_R_pin_adc_dataBit_0_3=_R_Quarter_VDD_dataBit_411=_R_Quarter_VDD_dataBit_0_3=0x00;            
+    _R_pin_adc_dataBit_411=0x00 ;
+    _R_pin_adc_dataBit_0_3=0x00 ;
+    _R_Quarter_VDD_dataBit_411=0x00;
+    _R_Quarter_VDD_dataBit_0_3=0x00;            
+
+    _FadcRead_pin(8);					// executing AIN4 ADC converting 8 times
+    _R_pin_adc_dataBit_411 <<= 4;					// _R_pin_adc_dataBit_411 shift left 4 bit
+    _R_pin_adc_dataBit_0_3 &= 0xF0;				// Only get Bit7~4
+    _R_pin_adc_dataBit_411 += _R_pin_adc_dataBit_0_3;		// _R_pin_adc_dataBit_411 + _R_pin_adc_dataBit_0_3
+    _R_pin_adc_dataBit_411 >>=3;					// _R_pin_adc_dataBit_411 dividing 8
+
+    _FadcRead_Quarter_VDD(8);			// executing 1/4VDD input channel ADC converting 8 times
+    _R_Quarter_VDD_dataBit_411 <<= 4;			// _R_Quarter_VDD_dataBit_411 shift left 4 bit
+    _R_Quarter_VDD_dataBit_0_3 &= 0xF0;		// Only get Bit7~4
+    _R_Quarter_VDD_dataBit_411 += _R_Quarter_VDD_dataBit_0_3; //// _R_Quarter_VDD_dataBit_411 + _R_Quarter_VDD_dataBit_0_3
+    _R_Quarter_VDD_dataBit_411 >>=3;			// _R_Quarter_VDD_dataBit_411 dividing 8
+} // _FmainLoop
+
+
+//----- Sub-Routine ----- 
+void _FadcRead_pin(char count)
+{
+    char i;
+    ADMD  = 0x90 | C_ADC_PA4;				// Select AIN4(PA4) pad as ADC input
+    for(i=1;i<=count;i++)
+    {     			 
+        ADMDbits.START = 1;					// Start a ADC conversion session
+        _F_wait_adc_conver_end();							// Waiting for ADC conversion complet	
+        _R_pin_adc_dataBit_0_3 += ( 0x0F & ADR); 
+        _R_pin_adc_dataBit_411    += ADD; 
     }
 }
 
-//----- Sub-Routine ----- 
-void _F_AIN0_Convert(char count)
+void _FadcRead_Quarter_VDD(char count)
 {
-  	char i;
-  	ADMD  = 0x90 | C_ADC_PA0;				// Select AIN0(PA0) pad as ADC input
-  	for(i=1;i<=count;i++)
-  	{     			 
-  	 ADMDbits.START = 1;					// Start a ADC conversion session
-  	 _F_wait_eoc();							// Waiting for ADC conversion complet	
-  	 _R_AIN0_DATA_LB += ( 0x0F & ADR); 
-  	 _R_AIN0_DATA    += ADD; 
-  	}
+    char i;
+    ADMD  = 0x90 | C_Quarter_VDD;			// Select internal 1/4VDD as ADC input
+    for(i=1;i<=count;i++)
+    {     			 
+        ADMDbits.START = 1;					// Start a ADC conversion session
+        _F_wait_adc_conver_end();							// Waiting for ADC conversion complet	
+        _R_Quarter_VDD_dataBit_0_3 += ( 0x0F & ADR); 
+        _R_Quarter_VDD_dataBit_411    += ADD; 
+    }
 }
 
-void _F_Quarter_VDD_Convert(char count)
+void _F_wait_adc_conver_end(void)
 {
-  	char i;
-  	ADMD  = 0x90 | C_Quarter_VDD;			// Select internal 1/4VDD as ADC input
-  	for(i=1;i<=count;i++)
-  	{     			 
-  	 ADMDbits.START = 1;					// Start a ADC conversion session
-  	 _F_wait_eoc();							// Waiting for ADC conversion complet	
-  	 _R_Quarter_VDD_DATA_LB += ( 0x0F & ADR); 
-  	 _R_Quarter_VDD_DATA    += ADD; 
-  	}
+    while(ADMDbits.EOC==0)
+        ;
 }
 
-void _F_wait_eoc(void)
+void _FdelayMS(int count)
 {
-   while(ADMDbits.EOC==0)
-   ;
-}
-
-void _delay(int count)
-{
-	int i;
-	for(i=1;i<=count;i++)
-	;
+    int i;
+    for(i=1;i<=count;i++)
+        ;
 }
