0866aaa3d4f2672b8790457ddd879f3c2e66417e
Your Name <you@example.com>
2019-01-14 20:52:38 +0800

    _2019_0114__2052pm

--- test01_local/main.c
+++ test01_local/main.c
@@ -9,6 +9,16 @@
  * A2 as 600mv_up
  * A5 as 600mv_down (OD)
  */
+/* AT8 - B62D sot23-6pin
+ * 1 : PA4 /AIN4 / EX_CKIO / SCK       --------> --------> --------> --------> --------> PA4 : ADC in, check 600mv wave
+ * 2 : VSS / GND
+ * 3 : PA5 / RSTb / VPP                
+ * 4 : PB3 / AIN3 / PWM1 / BZ1 / CMPO / SDO      --------> --------> --------> --------> PB3 : PD indecator ; 0 -> busying, 1 -> ok.
+ * 5 : VDD / VCC
+ * 6 : PA2 / AIN2 / PWM3 / BZ3 / SDI   --------> --------> --------> --------> --------> PA2 : 0 -> not 600mv , 1 -> 600mvIng
+ * A2 as 600mv_up
+ * A5 as 600mv_down (OD)
+ */
 void _Fanalyze_L(void);
 void _Fanalyze_H(void);
 void _Fanalyze_M(void);
@@ -18,49 +28,29 @@ void _Fanalyze_M(void);
 
 #define UPDATE_REG(x)	__asm__("MOVR _" #x ",F")
 
-unsigned char _bitB3  ;
-unsigned char _bitA25 ;
-#define _bitA2 _bitA25 
-#define _bitA5 _bitA25 
-
-//#define _action_01_toggle_B3       { PORTB ^= C_PB3_Input ; } // less byte , but unstable , for the B3 bit maybe force change by the ext voltage.
-//#define _action_01_toggle_B3       { _bitB3 = ! _bitB3 ; PORTBbits.PB3 = _bitB3 ; } //  more bytes : 20 bytes
-//#define _action_01_toggle_B3       { _bitB3 = ! _bitB3 ; if (_bitB3 ) PORTBbits.PB3 = 1; else PORTBbits.PB3 = 0; } //  more bytes : 27 bytes : 0x2c-0x47
-#define _action_01_toggle_B3       { _bitB3 ^= 0x01 ; if (_bitB3 ) PORTBbits.PB3 = 1; else PORTBbits.PB3 = 0; } //  more bytes : 10 bytes :0x2c-0x35
-
-#define _action_21_set_600mv__onA2 { PORTAbits.PA2 = 1; _set_A2_as_output ; PORTAbits.PA2 = 1; }
-#define _action_21_set_600mv_offA2 { _set_A2_as__input }
-#define _action_22_set_600mv__onA5 { _set_A5_OD__on }
-#define _action_22_set_600mv_offA5 { _set_A5_OD_off }
-
-#define _action_11_set_600mv__on   { _action_21_set_600mv__onA2 ; _action_22_set_600mv__onA5 ; }
-#define _action_11_set_600mv_off   { _action_21_set_600mv_offA2 ; _action_22_set_600mv_offA5 ; }
-//#define _action_11_set_600mv__on   { _action_21_set_600mv__onA2 ; }
-//#define _action_11_set_600mv_off   { _action_21_set_600mv_offA2 ; }
-//#define _action_11_set_600mv__on   { _action_22_set_600mv__onA5 ; }
-//#define _action_11_set_600mv_off   { _action_22_set_600mv_offA5 ; }
-
-#define _set_A5_ctrl_1      { IOSTA |=   C_PA5_Input  ; }       
-#define _set_A5_ctrl_0      { IOSTA &= (~C_PA5_Input) ; } 
-#define _set_A5_data_1      { _bitA5 = 0x1  ; PORTAbits.PA5 = 1; }
-#define _set_A5_data_0      { _bitA5 = 0x0  ; PORTAbits.PA5 = 0; }
-#define _set_A5_data_1only  {                 PORTAbits.PA5 = 1; }
-#define _set_A5_data_0only  {                 PORTAbits.PA5 = 0; }
-
-#define _set_A2_data_1      { _bitA2 = 0x1  ; PORTAbits.PA2 = 1; }
-#define _set_A2_data_0      { _bitA2 = 0x0  ; PORTAbits.PA2 = 0; }
-#define _set_A2_data_1only  {                 PORTAbits.PA2 = 1; }
-#define _set_A2_data_0only  {                 PORTAbits.PA2 = 0; }
-
 #define _set_A2_ctrl_1      { IOSTA |=   C_PA2_Input  ; }       
 #define _set_A2_ctrl_0      { IOSTA &= (~C_PA2_Input) ; }       
+#define _set_B3_ctrl_1      { IOSTB |=   C_PB3_Input  ; }       
+#define _set_B3_ctrl_0      { IOSTB &= (~C_PB3_Input) ; }       
 
-#define _set_A5_OD__on        _set_A5_data_0only
-#define _set_A5_OD_off        _set_A5_data_1only
-#define _set_A5_as_output      _set_A5_ctrl_0
+// 1 -> input ; 0 -> output             
+#define _set_A2_as_output   _set_A2_ctrl_0      
+#define _set_B3_as_output   _set_B3_ctrl_0      
 
-#define _set_A2_as__input   _set_A2_ctrl_1      // 1 -> input ; 0 -> output             
-#define _set_A2_as_output   _set_A2_ctrl_0      // 1 -> input ; 0 -> output             
+#define _set_PA2_1only      { PORTAbits.PA2 = 1 ; }
+#define _set_PA2_0only      { PORTAbits.PA2 = 0 ; }
+#define _set_PB3_1only      { PORTBbits.PB3 = 1 ; }
+#define _set_PB3_0only      { PORTBbits.PB3 = 0 ; }
+
+#define _set_PA2_1          { _set_A2_as_output ; _set_PA2_1only ; }
+#define _set_PA2_0          { _set_A2_as_output ; _set_PA2_0only ; }
+#define _set_PB3_1          { _set_B3_as_output ; _set_PB3_1only ; }
+#define _set_PB3_0          { _set_B3_as_output ; _set_PB3_0only ; }
+
+#define _action_11_set_600mv__on   { _set_PA2_1     ; }
+#define _action_11_set_600mv_off   { _set_PA2_0     ; }
+
+#define _action_12_set_PD_finish   { _set_PB3_1     ; }
 
 void _FadcRead_pin(void);
 void _Fanalyze_State(void);
@@ -75,40 +65,25 @@ unsigned int  _RadcL ;
 unsigned char _RadcREAL ;
 unsigned char _R_state ;
 
+unsigned char _PD_state ; 
+#define _PD__init           0
+#define _PD__600mvIng       1 
+#define _PD__600mvEnd       2
+#define _PD__finish         3
+
 void main(void)
 {
-
-    //----- Initial GPIO-----
-    IOSTA = C_PA_Input;						// Set PortA as input port
-    PORTA = 0xFF;							// PortA Data Register = 0xFF
     INTE  = 0x00;							// INTE = 0x00
 
-    _bitB3  = 0 ;
-    _bitA25 = 0 ;
+    //----- Initial GPIO-----
+    //IOSTA = C_PA_Input & ( ~C_PA2_Input ) ;    // Set PortA as input port , except A2 is output
+    _set_PA2_0 ;
 
     // B3 as the indecator
-    IOSTB = C_PB_Input & ( ~ C_PB3_Input ) ;// Set PortB as input port , except PortB3 is output
-    //IOSTB = (IOSTB | C_PB_Input) & ( ~ C_PB3_Input ) ;// Set PortB as input port , except PortB3 is output
+    //IOSTB = C_PB_Input & ( ~C_PB3_Input ) ;// Set PortB as input port , except PortB3 is output
+    _set_PB3_0 ;
 
-    _set_A2_data_1 ;
-#if 0
-    _set_A2_as_output ;
-    _set_A2_data_1 ;
-    _set_A2_as__input ;
-
-    _set_A2_data_0 ;
-    _set_A2_as_output ;
-    _set_A2_data_0 ;
-    _set_A2_as__input ;
-
-    _set_A2_data_1 ;
-    _set_A2_as_output ;
-    _set_A2_data_1 ;
-    _set_A2_as__input ;
-#endif
-
-    _set_A5_OD_off ;
-    _set_A5_as_output ;
+    _PD_state = _PD__init ;
 
     //----- Initial ADC-----	  
     ADMD  = C_ADC_En | C_ADC_CH_Dis | C_ADC_PA4 ;	// Enable ADC power, Disable global ADC input channel, Select PA4 pad as ADC input (SFR "ADMD")
@@ -151,7 +126,6 @@ void _FmainLoop(void)
 
     _Fanalyze_State() ;
 
-    _action_01_toggle_B3   ;
 
 } // _FmainLoop
 
@@ -172,9 +146,24 @@ void _FmainLoop(void)
 #define _state6mvOKmax  2
 #define _stateOveride   (_state6mvOKmax + 1 )
 
+void _FafterPD600mvEnd(void ) {
+    unsigned char __ii01  ;
+
+    for ( __ii01 = 100 ; __ii01 > 0 ; __ii01 -- ) {
+    }
+    _PD_state = _PD__finish ;
+    while ( 1 ) {
+        _action_12_set_PD_finish ;
+    }
+} // _FafterPD600mvEnd
+
 void _Fanalyze_L(void){
     _action_11_set_600mv_off ;
     _R_state = 0 ;
+    if ( _PD__600mvIng == _PD_state ) {
+        _PD_state = _PD__600mvEnd ;
+        _FafterPD600mvEnd() ;
+    }
 } // _Fanalyze_L
 
 void _Fanalyze_M(void){
@@ -183,6 +172,7 @@ void _Fanalyze_M(void){
         _R_state = 0 ;
     } else
         if ( _R_state == _state6mvOKmax ) {
+            _PD_state = _PD__600mvIng ;
             _action_11_set_600mv__on ;
         } else 
         {
